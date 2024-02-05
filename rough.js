69599f74afd6711238cc7dc0c53da6312a42061f
Balkishan <143352599+balkishan69@users.noreply.github.com>
2024-02-05 12:49:51 +0530

    Delete rough.js

--- rough.js
+++
@@ -1,9 +0,0 @@
-const dogs = ["German shepherd", "chow chow", "Golden"];
-const fish = ["Dolphin", "whale"];
-dogs.push(fish);
-console.log(dogs);
-const animals = dogs.concat(fish);
-console.log(animals);
-
-const newAnimals = [...dogs, ...fish];
-console.log(newAnimals);
